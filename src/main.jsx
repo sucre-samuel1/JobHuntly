23aeb45c6c53e17e493d3ee63291b990439c25cb
PS Parwez <infoparwez200@gmail.com>
2025-03-19 15:40:09 +0530

    fix: resolve build issue

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import Layout from "./layout";
+import Layout from "./Layout";
 import Home from "./components/Home/Home";
 import Brands from "./components/Brands/Brands";
 import Categories from "./components/Categories/Categories";
