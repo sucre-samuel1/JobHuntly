2074149336b281d7da913cd0cc8e5ba14813fb10
PS Parwez <infoparwez200@gmail.com>
2025-10-11 17:32:30 +0530

    fix: adjust brand logo

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -14,15 +14,22 @@ const brands = [
 
 const Brands = () => {
   return (
-    <section className="py-10 ">
+    <section className="py-16 ">
       <div className="container">
-        <p className="font-normal text-[18px] leading-7 text-[#202430] opacity-50 text-left">
+        <h3 className="font-normal text-[18px] leading-7 text-[#202430] opacity-50 text-left">
           Companies we helped grow
-        </p>
-        <div className="w-full mt-5 ">
-          <div className="flex flex-wrap items-center justify-start gap-3 sm:justify-between">
+        </h3>
+        <div className="w-full mt-5  ">
+          <div className="flex flex-wrap justify-around items-center gap-4 sm:justify-between">
             {brands.map((brand, index) => (
-              <div key={index} className="w-24 ml-1 h-max">
+              <div
+                key={index}
+                className={`${
+                  index === 1 || index === 3 || index === 4
+                    ? "w-20 lg:w-24"
+                    : "w-24"
+                } ml-1 h-max`}
+              >
                 <img
                   className="md:h-[100px] h-12 opacity-80 object-contain"
                   src={brand.image}
